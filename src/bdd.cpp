aa443bfbc3508547ce9365d7da26fe81dfc8d91a
Augustin-FL <Augustin-FL@users.noreply.github.com>
2015-03-25 23:03:49 +0100

    added menu bar+main windows

--- src/bdd.cpp
+++ src/bdd.cpp
@@ -16,25 +16,23 @@ connexion_bdd::~connexion_bdd()
 	sqlite3_close(bdd);
 }
 
-int connexion_bdd::exec(const string &texte)
+requete_sql* connexion_bdd::exec(const string &texte)
 {
-	char *texte_erreur;
-	if(sqlite3_exec(bdd,(char*)&texte,NULL,0,&texte_erreur)!= SQLITE_OK);
+	requete_sql* req=new requete_sql(bdd,texte);
+	
+	if(req->fetch())
 	{
-		wxMessageBox("erreur",texte_erreur);//si il est impossible de les creer
-		sqlite3_free(texte_erreur);
+		return req;
 	}
 	return 0;
 }
 
-
-
-requete_prepare* connexion_bdd::prepare(const string &texte)
+requete_sql* connexion_bdd::prepare(const string &texte)
 {
-	return new requete_prepare(bdd,texte);
+	return new requete_sql(bdd,texte);
 }
 
-requete_prepare::requete_prepare(sqlite3*& bdd,const string& texte_requete)
+requete_sql::requete_sql(sqlite3*& bdd,const string& texte_requete)
 {	
 	bdd_private=bdd;
 	
@@ -46,28 +44,28 @@ requete_prepare::requete_prepare(sqlite3*& bdd,const string& texte_requete)
 	// a faire : compter le onmbre de : et de ;
 }
 
-int requete_prepare::bind(const string &cle,int valeur)
+int requete_sql::bind(const string &cle,int valeur)
 {
 	return sqlite3_bind_int(requete, sqlite3_bind_parameter_index(requete,cle.c_str()), valeur);
 }
 
 
-int requete_prepare::bind(const string &cle,const string &valeur)
+int requete_sql::bind(const string &cle,const string &valeur)
 {
 	return sqlite3_bind_text(requete,sqlite3_bind_parameter_index(requete,cle.c_str()), valeur.c_str(),strlen(valeur.c_str()),SQLITE_STATIC);
 }
 
-int requete_prepare::bind(int cle,int valeur)
+int requete_sql::bind(int cle,int valeur)
 {
 	return sqlite3_bind_int(requete,cle, valeur);
 }
 
-int requete_prepare::bind(int cle,const string &valeur)
+int requete_sql::bind(int cle,const string &valeur)
 {
 	return sqlite3_bind_text(requete,cle, valeur.c_str(),strlen(valeur.c_str()),SQLITE_STATIC);
 }
 
-int requete_prepare::fetch()
+int requete_sql::fetch()
 {
 	int resultat=sqlite3_step(requete);
 	
@@ -77,15 +75,11 @@ int requete_prepare::fetch()
 		types=new int[nb_colonnes+1];
 		int j;
 		
-		for(j=0;j<nb_colonnes;j++);
-			types[j]=sqlite3_column_type(requete,j);
-			
-		wxString a="";
-		a<<types[1];
-		wxMessageBox(a,"zz");
+		for(j=0;j<nb_colonnes;j++)	types[j]=sqlite3_column_type(requete,j);
 		
 		return 1;
 	}
+	else if(resultat==SQLITE_DONE) return 1;
 	else if(resultat==SQLITE_BUSY)
 	{
 			wxMessageBox("erreur", _T("Erreur ! SQLITE_BUSY"));
@@ -100,44 +94,19 @@ int requete_prepare::fetch()
 		
 	return 0;
 }
-int requete_prepare::getColumn_int(int numero)
+int requete_sql::getColumn_int(int numero)
 {
-	if(numero>nb_colonnes || numero<0 || types[numero]!=SQLITE_TEXT ) 
-	{
-		wxString a;
-	//	a<<types[numero];
-		//wxMessageBox(a,"zz");
-		return -1;
-	}
+	if(numero>nb_colonnes || numero<0 || types[numero]!=SQLITE_INTEGER) return -1;
+	
 	return sqlite3_column_int(requete,numero);
 }
-string requete_prepare::getColumn_text(int numero)
+string requete_sql::getColumn_text(int numero)
 {
-	if(numero>nb_colonnes || numero<0  || types[numero]!=SQLITE_TEXT)  return "erreur - tt";
-	return (char*)sqlite3_column_text(requete, numero);
+	if(numero>nb_colonnes || numero<0  || types[numero]!=SQLITE_TEXT)  return "erreur - GetColumn_text()";
+	return string((const char*)sqlite3_column_text(requete, numero));
 }
-double requete_prepare::getColumn_float(int numero)
+double requete_sql::getColumn_float(int numero)
 {
 	if(numero>nb_colonnes || numero<0  || types[numero]!=SQLITE_FLOAT)  return -1.0;
 	return sqlite3_column_double(requete,numero);
 }
-
-
-/*
-char* requete_prepare::getColumn(int numero)
-{
-	if(numero>nb_colonnes || numero<0) return (void)-1;
-	
-	if     (types[numero]==SQLITE_INTEGER) return ()sqlite3_column_int( requete,numero);
-	else if(types[numero]==SQLITE_FLOAT)   return (void)sqlite3_column_double(requete,numero);
-	else if(types[numero]==SQLITE_TEXT)    return (void)sqlite3_column_text(requete, numero);
-	else if(types[numero]==SQLITE_BLOB)    return (void)sqlite3_column_blob(requete, numero);
-	else return NULL;
-}*/
-//int 
-
-/*	
-sqlite3_prepare_v2(bdd,"SELECT COUNT(*) FROM adherents WHERE livre1!=-1 AND retour_livre1<=:retour_max OR livre2!=-1 AND retour_livre2<=:retour_max OR livre3!=-1 AND retour_livre3<=:retour_max",-1,&requete, NULL);
-		sqlite3_bind_int(requete,sqlite3_bind_parameter_index(requete,":retour_max"),time(NULL));//1413148270 <- timesamp octobre
-sqlite3_step(requete),SQLITE_ROW,bdd,position,""
-		*/
--- src/main.h
+++ src/main.h
@@ -12,16 +12,17 @@
 
 	enum 
 	{
+		PAS_CONNECTE=-1,
 		ELEVE=0,
 		PROF=1,
 		ADMIN=2,
 		
 	};
 
-	class requete_prepare : public wxString
+	class requete_sql : public wxString //connexion a la BDD
 	{
 		public:
-			requete_prepare(sqlite3 *&bdd,const string& texte);
+			requete_sql(sqlite3 *&bdd,const string& texte);
 			int bind(const string &cle,const string &valeur);
 			int bind(const string &cle,int valeur);
 			int bind(int cle,int valeur); 
@@ -37,7 +38,6 @@
 			int* types;
 			int nb_colonnes;
 	};
-	 
 	class connexion_bdd : public wxString
 	{
 		private:
@@ -46,8 +46,8 @@
 		public:
 			connexion_bdd();
 			~connexion_bdd();
-			int exec(const string &texte);
-			requete_prepare* prepare(const string &texte);
+			requete_sql* exec(const string &texte);
+			requete_sql* prepare(const string &texte);
 		
 	};
 	
@@ -62,21 +62,45 @@
 	};
 	DECLARE_APP(App_GestNote);
 	
+	
+	class Frame_principale: public wxFrame
+	{
+		private:
+			int type;
+			int id;
+			connexion_bdd* bdd;
+			bool veto_autorise;
+
+		public:
+			Frame_principale(connexion_bdd*& arg_bdd);
+			~Frame_principale(){};
+			void afficher_apres_login(int type_arg, int id_arg);
+			void onClose(wxCloseEvent &evenement);
+			void main_admin();
+			void main_eleve();
+			void main_prof();
+			void onQuit(wxCommandEvent &evenement);
+			
+		
+	};
 
 	class Frame_login : public wxFrame
 	{
 		public:
-			Frame_login(connexion_bdd*& arg_bdd);
+			Frame_login(Frame_principale*& parent, connexion_bdd*& arg_bdd);
 			virtual ~Frame_login(){};
 		
 		private:
 			wxTextCtrl *input_login,*input_mdp;
 			wxButton *bouton_valider, *bouton_annuler;
 			connexion_bdd* bdd;
+			int type;
+			Frame_principale* frame_parente;
 			
 			void onClick_valider(wxCommandEvent &evenement);
 			void onClick_annuler(wxCommandEvent &evenement);
 			void onChange(wxCommandEvent &evenement);
+			void onClose(wxCloseEvent &evenement);
 	};
 
 #endif
