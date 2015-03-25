aeca6828bdcf9d341c63413a012e758dc4961e17
gusfl@free.fr <gusfl@free.fr>
2015-03-25 23:03:49 +0100

    added menu bar+main windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "main.h"
 IMPLEMENT_APP(App_GestNote);
 
-Frame_login::Frame_login(connexion_bdd*& arg_bdd): wxFrame(NULL, wxID_ANY,_T("GestNote"),wxDefaultPosition,*(new wxSize(300,220)))
+Frame_login::Frame_login(Frame_principale*& parent, connexion_bdd*& arg_bdd): wxFrame(parent, wxID_ANY,_T("GestNote"),wxDefaultPosition,*(new wxSize(300,220)))
 {
 	wxPanel         *fenetre					= new wxPanel(this);
 	wxBoxSizer      *contenu_fenetre_sans_marge	= new wxBoxSizer(wxVERTICAL);
@@ -12,7 +12,7 @@ Frame_login::Frame_login(connexion_bdd*& arg_bdd): wxFrame(NULL, wxID_ANY,_T("Ge
 	wxStaticText	*texte_explicatif			= new wxStaticText(fenetre, -1, _T("\nVeuillez vous authentifier\npour accéder à votre espace GestNote.\n"));
 	wxStaticBoxSizer*conteneur_authentification	= new wxStaticBoxSizer(wxVERTICAL,fenetre,_T("Authentification : "));
 	
-	wxStaticText	*label_login				= new wxStaticText(fenetre, -1, _T("Login : ")); 				
+	wxStaticText	*label_login				= new wxStaticText(fenetre, -1, _T("Matricule : ")); 				
 	wxStaticText	*label_mdp					= new wxStaticText(fenetre, -1, _T("Mot de passe : ")); 				
 	
 	bouton_valider	= new wxButton(    fenetre, -1, _T("Valider"));
@@ -44,11 +44,16 @@ Frame_login::Frame_login(connexion_bdd*& arg_bdd): wxFrame(NULL, wxID_ANY,_T("Ge
 	
 	bouton_valider->Connect(wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(Frame_login::onClick_valider), NULL, this);
 	bouton_annuler->Connect(wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(Frame_login::onClick_annuler), NULL, this);
-	input_login->Connect( wxEVT_COMMAND_TEXT_UPDATED, wxCommandEventHandler(Frame_login::onChange), NULL, this);
-	input_mdp->Connect(   wxEVT_COMMAND_TEXT_UPDATED, wxCommandEventHandler(Frame_login::onChange), NULL, this);
-
+	input_login->Connect( 	wxEVT_COMMAND_TEXT_UPDATED, wxCommandEventHandler(Frame_login::onChange), NULL, this);
+	input_mdp->Connect( 	wxEVT_COMMAND_TEXT_UPDATED, wxCommandEventHandler(Frame_login::onChange), NULL, this);
+	this->Connect(			wxEVT_CLOSE_WINDOW,wxCloseEventHandler(Frame_login::onClose),NULL, this);
+	this->Show();
+	
 	bdd=arg_bdd;
+	frame_parente=parent;
+	type=PAS_CONNECTE;
 }
+
 
 void Frame_login::onChange(wxCommandEvent &evenement)
 {
@@ -63,46 +68,165 @@ void Frame_login::onChange(wxCommandEvent &evenement)
 void Frame_login::onClick_valider(wxCommandEvent &evenement)
 {
      
-	requete_prepare* req=bdd->prepare("SELECT count(*),type FROM login_centralise WHERE matricule=:matricule and mdp=:mdp");//WHERE matricule=:matricule AND mdp=:mdp
+	requete_sql* req=bdd->prepare("SELECT count(*),type FROM login_centralise WHERE matricule=:matricule and mdp=:mdp");//WHERE matricule=:matricule AND mdp=:mdp
 	
 	req->bind(":matricule",string(input_login->GetValue().mb_str())); //si une erreur survient a ce niveau : 
 	req->bind(":mdp",string(input_mdp->GetValue().mb_str()));//penser a utiliser le namespace std, ou a mettre std::string()
 
-	int resultat=req->fetch();
-	
-	if(resultat && req->getColumn_int(0)==1)
+	if(req->fetch() && req->getColumn_int(0)==1)
 	{
-		Hide();
-		wxString nombre;
-		nombre << req->getColumn_text(1);
-		wxMessageBox(nombre,_T("bienvenu !"));
-		Close();
+		type=req->getColumn_int(1);
+		this->Hide();
+		frame_parente->afficher_apres_login(type,req->getColumn_int(2));	
+		this->Close();
 	}
 	else 
 	{
 		wxMessageBox(_T("Identifients incorrects !"),_T("erreur"));
 		input_mdp->SetValue("");
-		Show();
 	}
-	
-	
-
     
 }
 
 void Frame_login::onClick_annuler(wxCommandEvent &evenement)
 {
-    int reponse=wxMessageBox(_T("Voulez vous vraiment quitter?"), _T("Quitter"), wxYES_NO | wxCANCEL);
-	if (reponse == wxYES) Close();
+	this->Close();
+}
+
+
+void Frame_login::onClose(wxCloseEvent &evenement)
+{
+		
+	if(evenement.CanVeto() && type==PAS_CONNECTE)
+	{
+		int reponse=wxMessageBox(_T("Voulez vous vraiment quitter?"), _T("Quitter"), wxYES_NO | wxCANCEL);
+		if (reponse == wxNO) 
+		{
+			evenement.Veto();	
+			return ;
+		}
+	}
+	
+	this->Destroy();
+	if(type==PAS_CONNECTE) frame_parente->Destroy();
+}
+
+
+
+
+
+
+
+Frame_principale::Frame_principale(connexion_bdd*& bdd_arg) : wxFrame(NULL, wxID_ANY,_T("GestNote"),wxDefaultPosition,*(new wxSize(800,800)))
+{
+	bdd=bdd_arg;
+	veto_autorise=true;
+}
+
+void Frame_principale::afficher_apres_login(int type_arg, int id_arg)
+{
+	type=type_arg;
+	id=id_arg;
+	
+	if(type==ELEVE)
+	{
+		//consulter les notes (tableau) +imprimer buletin de notes
+		//consulter son groupe
+		//voir (modifier?) contacts
+		
+//		wxStaticBoxSizer*conteneur_notes	= new wxStaticBoxSizer(wxVERTICAL,fenetre,_T("Notes : "));
+	//	contenu_fenetre_sans_marge->Add(conteneur_notes, 1, wxALIGN_CENTER_VERTICAL);
+		
+	}
+	else if(type==PROF)
+	{
+		//liste déroulante en haut.->matière
+		
+		//saisir notes
+		//changer groupe(?)
+		//si accord de l'admin : changer les notes
+	
+	}
+	else if(type==ADMIN) this->main_admin();
+	else 
+	{
+			wxMessageBox(_T("Erreur ! Type de personne inconnu"), _T("GestNote"));
+			veto_autorise=false;
+			Close(); 
+			return ;
+	}
+	
+	
+	this->Connect(wxEVT_CLOSE_WINDOW,wxCloseEventHandler(Frame_principale::onClose),NULL, this);
+	this->Show();
+}
+
+
+void Frame_principale::main_admin()
+{
+	wxMenuBar *barre_menu= new wxMenuBar();
+    wxMenu *menu_fichier = new wxMenu();
+	wxTextCtrl *input_login,*input_mdp;
+	
+	//saisir prof
+	//saisir éleve
+	//ajouter/supprimer/modifier eleve & prof 
+	wxPanel         *fenetre					= new wxPanel(this);
+	wxBoxSizer      *contenu_fenetre_sans_marge	= new wxBoxSizer(wxVERTICAL);
+	
+	input_mdp		= new wxTextCtrl(  fenetre, -1, _T(""),wxDefaultPosition,wxDefaultSize,wxTE_PASSWORD);
+	input_login		= new wxTextCtrl(  fenetre, -1, _T(""));
+
+	
+	fenetre->SetSizer(contenu_fenetre_sans_marge);
+	
+		
+	wxStaticBoxSizer*conteneur_ajout= new wxStaticBoxSizer(wxVERTICAL,fenetre,_T("Ajouter : "));
+	contenu_fenetre_sans_marge->Add(conteneur_ajout,0,  wxALIGN_TOP, 10); 
+//	contenu_
+	
+	conteneur_ajout->Add(input_login,1, 10);
+	conteneur_ajout->Add(input_mdp,1);
+	
+	
+	
+	
+	menu_fichier->Append(wxID_EXIT,	 _T("Quitter"));
+	barre_menu->Append(menu_fichier, _T("Fichier"));
+	this->SetMenuBar(barre_menu);
+	
+	this->Connect(wxID_EXIT, wxEVT_COMMAND_MENU_SELECTED, wxCommandEventHandler(Frame_principale::onQuit));
+}
+
+
+void Frame_principale::onQuit(wxCommandEvent &evenement)
+{
+	this->Close();
+}
+
+void Frame_principale::onClose(wxCloseEvent &evenement)
+{
+		
+	if(evenement.CanVeto())
+	{
+		int reponse=wxMessageBox(_T("Voulez vous vraiment quitter?"), _T("Quitter"), wxYES_NO | wxCANCEL);
+		if (reponse == wxNO)
+		{
+			evenement.Veto();
+			return ;
+		}
+	}
+	 this->Destroy();
 }
 
 bool App_GestNote::OnInit()
 {
+	
 	bdd=new connexion_bdd();
-	Frame_login *login=new Frame_login(bdd);
-	login->Show();
+	Frame_principale *frame_principale=new Frame_principale(bdd);
 	
-	
+	Frame_login *login=new Frame_login(frame_principale,bdd);
+	login->Show();//pour éviter un warning "unused variable"
  
     return true;
 }
