a50f5af8a72c35ff810b8365637fd2f0eb33acfb
krnruth <krnruth91@gmail.com>
2015-04-12 20:18:32 +0200

    Création du front professeur.cpp

--- src/login.cpp
+++ src/login.cpp
@@ -1,125 +1,125 @@
 #include "main.h"
 
 #ifndef __WXMSW__
-	#include "ressources.xpm"// ressources pour linux (le logo etc...)
+#include "ressources.xpm"// ressources pour linux (le logo etc...)
 #endif
 
 
 Frame_login::Frame_login( connexion_bdd*& arg_bdd): wxFrame(NULL, wxID_ANY,_T("GestNotes"),wxDefaultPosition,*(new wxSize(300,220)))
 {
-	SetIcon(wxICON(icone)); 
-	frame_actuelle=this;
-	wxPanel         *fenetre					= new wxPanel(this); // un "panel" est la fenètre en elle même
-	wxBoxSizer      *contenu_fenetre_sans_marge	= new wxBoxSizer(wxVERTICAL); // les boxSizer et StaticBoxSizer sont l'équivalent des "divs" en HTML
-	wxBoxSizer		*conteneur_horisontal_login	= new wxBoxSizer(wxHORIZONTAL);
-	wxBoxSizer		*conteneur_horisontal_mdp 	= new wxBoxSizer(wxHORIZONTAL);
-	wxBoxSizer 		*conteneur_boutons			= new wxBoxSizer(wxHORIZONTAL);
-	
-	wxStaticText	*texte_explicatif			= new wxStaticText(fenetre, -1, _T("\nVeuillez vous authentifier\npour accéder à votre espace GestNotes.\n"));
-	wxStaticBoxSizer*conteneur_authentification	= new wxStaticBoxSizer(wxVERTICAL,fenetre,_T("Authentification : "));
-	
-	wxStaticText	*label_login				= new wxStaticText(fenetre, -1, _T("Matricule : ")); 	//Du texte
-	wxStaticText	*label_mdp					= new wxStaticText(fenetre, -1, _T("Mot de passe : ")); 				
-	
-	bouton_valider	= new wxButton(    fenetre, -1, _T("Valider"));// Des champs d'input
-	bouton_annuler 	= new wxButton(    fenetre, -1, _T("Quitter"));
-	input_mdp		= new wxTextCtrl(  fenetre, -1, _T(""),wxDefaultPosition,wxDefaultSize,wxTE_PASSWORD);// un input password
-	input_login		= new wxTextCtrl(  fenetre, -1, _T(""));
-	
-	fenetre->SetSizer(contenu_fenetre_sans_marge);
+    SetIcon(wxICON(icone));
+    frame_actuelle=this;
+    wxPanel         *fenetre					= new wxPanel(this); // un "panel" est la fenètre en elle même
+    wxBoxSizer      *contenu_fenetre_sans_marge	= new wxBoxSizer(wxVERTICAL); // les boxSizer et StaticBoxSizer sont l'équivalent des "divs" en HTML
+    wxBoxSizer		*conteneur_horisontal_login	= new wxBoxSizer(wxHORIZONTAL);
+    wxBoxSizer		*conteneur_horisontal_mdp 	= new wxBoxSizer(wxHORIZONTAL);
+    wxBoxSizer 		*conteneur_boutons			= new wxBoxSizer(wxHORIZONTAL);
 
-	
-	contenu_fenetre_sans_marge->Add(texte_explicatif, 			1, wxALIGN_CENTER);//la fenetre contient : 
-	contenu_fenetre_sans_marge->Add(conteneur_authentification, 1, wxALIGN_CENTER);//un texte explicatif, la zone de
-	contenu_fenetre_sans_marge->Add(conteneur_boutons, 			1, wxALIGN_CENTER|wxTOP, 10);//login, et les boutons
-	
-	conteneur_authentification->Add(conteneur_horisontal_login, 1, wxDOWN,5);//on dit que la zone d'authentification contient 
-	conteneur_authentification->Add(conteneur_horisontal_mdp,   1 );//un login et un mot de passe
-	
-	conteneur_horisontal_login->Add(label_login, 1);//la zone login contient un label pour le login, et un champ d'input.
-	conteneur_horisontal_login->Add(input_login, 1);
-	conteneur_horisontal_mdp->Add(label_mdp, 1);
-	conteneur_horisontal_mdp->Add(input_mdp, 1);
-	
-	conteneur_boutons->Add(bouton_valider,1);//et il y a 2 boutons : valider et annuler
-	conteneur_boutons->Add(bouton_annuler,1);
+    wxStaticText	*texte_explicatif			= new wxStaticText(fenetre, -1, _T("\nVeuillez vous authentifier\npour accéder à votre espace GestNotes.\n"));
+    wxStaticBoxSizer*conteneur_authentification	= new wxStaticBoxSizer(wxVERTICAL,fenetre,_T("Authentification : "));
 
-	bouton_valider->Disable();
-	
-	input_mdp->MoveAfterInTabOrder(input_login);//l'ordre des champs avec l'appui sur "tab"
-	bouton_annuler->MoveAfterInTabOrder(input_mdp);
-	
-	bouton_valider->Bind(wxEVT_BUTTON, 			 &Frame_login::onClick_valider, this);
-	bouton_annuler->Bind(wxEVT_BUTTON, 			 &Frame_login::onClick_annuler,this);
-	input_login->Bind(wxEVT_COMMAND_TEXT_UPDATED,&Frame_login::onChange,this);
-	input_mdp->Bind(  wxEVT_COMMAND_TEXT_UPDATED,&Frame_login::onChange, this);
-	this->Bind(		  wxEVT_CLOSE_WINDOW,		 &Frame_login::onClose, this);
-	this->Show();
+    wxStaticText	*label_login				= new wxStaticText(fenetre, -1, _T("Matricule : ")); 	//Du texte
+    wxStaticText	*label_mdp					= new wxStaticText(fenetre, -1, _T("Mot de passe : "));
 
-	bdd=arg_bdd;
-	type=PAS_CONNECTE;
+    bouton_valider	= new wxButton(    fenetre, -1, _T("Valider"));// Des champs d'input
+    bouton_annuler 	= new wxButton(    fenetre, -1, _T("Quitter"));
+    input_mdp		= new wxTextCtrl(  fenetre, -1, _T(""),wxDefaultPosition,wxDefaultSize,wxTE_PASSWORD);// un input password
+    input_login		= new wxTextCtrl(  fenetre, -1, _T(""));
+
+    fenetre->SetSizer(contenu_fenetre_sans_marge);
+
+
+    contenu_fenetre_sans_marge->Add(texte_explicatif, 			1, wxALIGN_CENTER);//la fenetre contient :
+    contenu_fenetre_sans_marge->Add(conteneur_authentification, 1, wxALIGN_CENTER);//un texte explicatif, la zone de
+    contenu_fenetre_sans_marge->Add(conteneur_boutons, 			1, wxALIGN_CENTER|wxTOP, 10);//login, et les boutons
+
+    conteneur_authentification->Add(conteneur_horisontal_login, 1, wxDOWN,5);//on dit que la zone d'authentification contient
+    conteneur_authentification->Add(conteneur_horisontal_mdp,   1 );//un login et un mot de passe
+
+    conteneur_horisontal_login->Add(label_login, 1);//la zone login contient un label pour le login, et un champ d'input.
+    conteneur_horisontal_login->Add(input_login, 1);
+    conteneur_horisontal_mdp->Add(label_mdp, 1);
+    conteneur_horisontal_mdp->Add(input_mdp, 1);
+
+    conteneur_boutons->Add(bouton_valider,1);//et il y a 2 boutons : valider et annuler
+    conteneur_boutons->Add(bouton_annuler,1);
+
+    bouton_valider->Disable();
+
+    input_mdp->MoveAfterInTabOrder(input_login);//l'ordre des champs avec l'appui sur "tab"
+    bouton_annuler->MoveAfterInTabOrder(input_mdp);
+
+    bouton_valider->Bind(wxEVT_BUTTON, 			 &Frame_login::onClick_valider, this);
+    bouton_annuler->Bind(wxEVT_BUTTON, 			 &Frame_login::onClick_annuler,this);
+    input_login->Bind(wxEVT_COMMAND_TEXT_UPDATED,&Frame_login::onChange,this);
+    input_mdp->Bind(  wxEVT_COMMAND_TEXT_UPDATED,&Frame_login::onChange, this);
+    this->Bind(		  wxEVT_CLOSE_WINDOW,		 &Frame_login::onClose, this);
+    this->Show();
+
+    bdd=arg_bdd;
+    type=PAS_CONNECTE;
 }
 
 
 void Frame_login::onChange(wxCommandEvent &evenement)
 {
-	if(input_login->IsEmpty() ||input_mdp->IsEmpty()) bouton_valider->Disable();
-	else 
-	{
-		bouton_valider->Enable();	
-		bouton_valider->SetDefault();
-	}
+    if(input_login->IsEmpty() ||input_mdp->IsEmpty()) bouton_valider->Disable();
+    else
+    {
+        bouton_valider->Enable();
+        bouton_valider->SetDefault();
+    }
 }
 
 void Frame_login::onClick_valider(wxCommandEvent &evenement)
 {
-	requete_sql* req=bdd->prepare("SELECT count(*),type FROM login_centralise WHERE matricule=:matricule and mdp=:mdp");//WHERE matricule=:matricule AND mdp=:mdp
-	
-	req->bind(":matricule",string(input_login->GetValue().mb_str())); //si une erreur survient a ce niveau : 
-	req->bind(":mdp",string(input_mdp->GetValue().mb_str()));//penser a utiliser le namespace std, ou a mettre std::string()
+    requete_sql* req=bdd->prepare("SELECT count(*),type FROM login_centralise WHERE matricule=:matricule and mdp=:mdp");//WHERE matricule=:matricule AND mdp=:mdp
 
-	if(req->fetch() && req->getColumn_int(0)==1)
-	{
-		type=req->getColumn_int(1);
-		int matricule=wxAtoi(input_login->GetValue());
-		
-		req->closeCursor();
-		this->Hide();
-		
-		if(type==ELEVE) frame_enfant=new Frame_eleve(this,matricule,bdd);
-		else if(type==PROF) frame_enfant=new Frame_prof(this,matricule,bdd);
-		else if(type==ADMIN) frame_enfant=new Frame_admin(this,matricule,bdd);
-		else
-		{
-			wxMessageBox(_T("Erreur ! Type de personne inconnu"), _T("GestNotes"));
-			this->Close();
-		}
-	}
-	else 
-	{
-		req->closeCursor();
-		wxMessageBox(_T("Identifients incorrects !"),_T("erreur"));
-		input_mdp->SetValue("");
-	}
+    req->bind(":matricule",string(input_login->GetValue().mb_str())); //si une erreur survient a ce niveau :
+    req->bind(":mdp",string(input_mdp->GetValue().mb_str()));//penser a utiliser le namespace std, ou a mettre std::string()
+
+    if(req->fetch() && req->getColumn_int(0)==1)
+    {
+        type=req->getColumn_int(1);
+        int matricule=wxAtoi(input_login->GetValue());
+
+        req->closeCursor();
+        this->Hide();
+
+        if(type==ELEVE) frame_enfant=new Frame_eleve(this,matricule,bdd);
+        else if(type==PROF) frame_enfant=new Frame_prof(this,matricule,bdd);
+        else if(type==ADMIN) frame_enfant=new Frame_admin(this,matricule,bdd);
+        else
+        {
+            wxMessageBox(_T("Erreur ! Type de personne inconnu"), _T("GestNotes"));
+            this->Close();
+        }
+    }
+    else
+    {
+        req->closeCursor();
+        wxMessageBox(_T("Identifients incorrects !"),_T("erreur"));
+        input_mdp->SetValue("");
+    }
 }
 
 void Frame_login::onClick_annuler(wxCommandEvent &evenement)
 {
-	this->Close();
+    this->Close();
 }
 
 
 void Frame_login::onClose(wxCloseEvent &evenement)
 {
-		
-	if(evenement.CanVeto() && type==PAS_CONNECTE)
-	{
-		int reponse=wxMessageBox(_T("Voulez vous vraiment quitter?"), _T("Quitter"), wxYES_NO | wxCANCEL);
-		if (reponse == wxNO) 
-		{
-			evenement.Veto();	
-			return ;
-		}
-	}
-	this->Destroy();
+
+    if(evenement.CanVeto() && type==PAS_CONNECTE)
+    {
+        int reponse=wxMessageBox(_T("Voulez vous vraiment quitter?"), _T("Quitter"), wxYES_NO | wxCANCEL);
+        if (reponse == wxNO)
+        {
+            evenement.Veto();
+            return ;
+        }
+    }
+    this->Destroy();
 }
--- src/professeur.cpp
+++ src/professeur.cpp
@@ -2,26 +2,45 @@
 
 Frame_prof::Frame_prof(Frame_login* parent,int& matricule,connexion_bdd*& bdd) : Frame_principale(parent,matricule,bdd)
 {
-	
-	this->SetSize(wxDefaultCoord,wxDefaultCoord,770,625);
-	
+	this->SetSize(wxDefaultCoord,wxDefaultCoord,400,400);
+    wxBoxSizer *sizer_principal = new wxBoxSizer(wxVERTICAL);
+
+    wxArrayString texte_select;
+    texte_select.Add(_T("Maths Du réel"));
+
 	wxPanel *fenetre = new wxPanel(this);
+    wxStaticBoxSizer *texte_conteneur_matiere 	= new wxStaticBoxSizer(wxHORIZONTAL,fenetre,_T("Choisir la matière : "));
+	wxChoice  *liste_matiere	= new wxChoice (fenetre, -1, wxDefaultPosition,wxDefaultSize,texte_select);
+	texte_conteneur_matiere->Add(liste_matiere);
+	wxStaticText *texte=new wxStaticText(fenetre, -1, _T("Gestion des notes")); // boite de choix pour les notes
+	sizer_principal->Add(texte,0,wxALIGN_CENTER);
+	sizer_principal->Add(liste_matiere,0,wxALIGN_CENTER|wxEXPAND|wxRIGHT|wxLEFT,10);
 
-	wxBoxSizer *sizer_principal = new wxBoxSizer(wxVERTICAL);
 
-	
-	
-	
+	wxBoxSizer *sizer_note= new wxBoxSizer(wxHORIZONTAL);
+    wxStaticBoxSizer *texte_conteneur_note 	= new wxStaticBoxSizer(wxHORIZONTAL,fenetre,_T("Prof : "));
+	wxButton *button_ajouter= new wxButton(fenetre, -1, _T("Ajouter"));
+	wxButton *button_modifier= new wxButton(fenetre, -1, _T("Modifier"));
+	wxButton *button_supprimer= new wxButton(fenetre, -1, _T("Supprimer"));
+
+	sizer_principal->Add(sizer_note,0, wxALIGN_CENTER,0);
+
+	sizer_note->Add(texte_conteneur_note,1,wxALIGN_CENTER|wxEXPAND,0);
+	texte_conteneur_note->Add(button_ajouter,0,wxALIGN_CENTER,5);
+	texte_conteneur_note->Add(button_modifier,0,wxALIGN_CENTER,5);
+	texte_conteneur_note->Add(button_supprimer,0,wxALIGN_CENTER,5);
+
 	fenetre->SetSizer(sizer_principal);
+
 	this->SetStatusText(_T("GestNotes - Accès Professeur"));
 	this->Show();
-	
+
 	//liste déroulante en haut.->matière
 
 			//saisir notes
-			//changer groupe(?)
+			//changer groupe(NON)
 			//si accord de l'admin : changer les notes
-			//ecrire des commentaires pour les buletins
+			//ecrire des commentaires pour les bulletins
 }
 
 
