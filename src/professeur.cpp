6d52c851a8db5d4b10ee8a8b4a382e221da0df59
gusfl@free.fr <gusfl@free.fr>
2015-05-01 23:15:42 +0200

    mark are editable by teacher now

--- src/professeur.cpp
+++ src/professeur.cpp
@@ -52,16 +52,16 @@ Frame_prof::Frame_prof(Frame_login* parent,int& matricule,connexion_bdd*& bdd) :
 	
 	wxStaticText *texte_header=new wxStaticText(fenetre, -1, _T("Gestion des notes")); // boite de choix pour les notes
 	
-	wxDataViewListCtrl   *liste_notes=new wxDataViewListCtrl(fenetre,-1, wxDefaultPosition, wxDefaultSize);
+	liste_notes=new wxDataViewListCtrl(fenetre,-1, wxDefaultPosition, wxDefaultSize);
 	
 	liste_matieres->SetSelection(0);
 	liste_classes->SetSelection(0);
 	
-	liste_notes->AppendToggleColumn(_T("Élève"));
-	liste_notes->AppendTextColumn(_T("CE"));
-	liste_notes->AppendTextColumn(_T("TAI"));
-	liste_notes->AppendTextColumn(_T("DE"));
-	liste_notes->AppendTextColumn(_T("Commentaires"));
+	liste_notes->AppendTextColumn(_T("Élève"));
+	liste_notes->AppendTextColumn(_T("CE"),wxDATAVIEW_CELL_EDITABLE);
+	liste_notes->AppendTextColumn(_T("TAI"),wxDATAVIEW_CELL_EDITABLE);
+	liste_notes->AppendTextColumn(_T("DE"),wxDATAVIEW_CELL_EDITABLE);
+	liste_notes->AppendTextColumn(_T("Commentaires"),wxDATAVIEW_CELL_EDITABLE);
 			
 	req=bdd->prepare("select eleve.id,eleve.nom,eleve.prenom, 0 as pas_de_note, notes.note,notes.type_note from eleve 	\
 															left outer join notes on notes.id_eleve=eleve.id 			\
@@ -76,27 +76,38 @@ Frame_prof::Frame_prof(Frame_login* parent,int& matricule,connexion_bdd*& bdd) :
 	
 	std::map<int,int> liste_eleves;
 	std::map<int,int>::iterator it;
+	wxVector<wxVariant> ligne;//wxWidgets à besoin d'un vecteur pour créer une ligne. Ce vecteurs doit contenir les champs des colonnes de cette ligne
+	
 	int position_y;
 	wxString texte_note;
 	
+	for(int i=0;i<5;i++) ligne.push_back(wxVariant(""));
+    
 	while(req->fetch())
 	{
 		texte_note="";
 		
 		it=liste_eleves.find(req->getColumn_int(0));//si la valeur n'est pas trouvée, it sera = a liste_eleves.end()
-		//if(it== liste_eleves.end()) liste_eleves[req->getColumn_int(0)]= liste_notes->InsertItem(liste_eleves.size()+1,string(req->getColumn_text(2))+" "+string(req->getColumn_text(1)));	//if(l'id de la matiere n'est pas dans le vector) vector[id] = la_position_ou_on_ajoute_cette_matiere
-
+		if(it== liste_eleves.end())//if(l'id de la matiere n'est pas dans le vector) vector[id] = la_position_ou_on_ajoute_cette_matiere
+		{
+			liste_eleves[req->getColumn_int(0)]=liste_eleves.size()-1;
+			
+			ligne[0]=wxVariant(string(req->getColumn_text(2))+" "+string(req->getColumn_text(1)));//on effectue l'ajout de la ligne
+			liste_notes->AppendItem(ligne);	
+		}
+		
 		if(req->getColumn_int(3)==0)//si il y a une note a afficher
 		{
 			position_y=req->getColumn_int(5);
 			texte_note<<req->getColumn_int(4);
 
-//			liste_notes->SetItem(liste_eleves[req->getColumn_int(0)], position_y,texte_note);
+			liste_notes->SetTextValue(texte_note,liste_eleves[req->getColumn_int(0)],position_y);
 			//if(il y a une note a afficher) on affiche la note en (position_x/liste_matiere; position_y/le_type_de_note)
 		}
 	}
 	req->closeCursor();
-		
+	liste_notes->Bind(wxEVT_DATAVIEW_ITEM_ACTIVATED, &Frame_prof::onDbclick_notes,this);
+	liste_notes->Bind(wxEVT_DATAVIEW_ITEM_VALUE_CHANGED, &Frame_prof::onChange_notes,this);
 	
 	sizer_principal->Add(texte_header,0,wxALIGN_CENTER);
 	sizer_principal->Add(conteneur_choix_matiere_classe,0,wxALIGN_CENTER);
@@ -121,6 +132,24 @@ Frame_prof::Frame_prof(Frame_login* parent,int& matricule,connexion_bdd*& bdd) :
 			//ecrire des commentaires pour les bulletins
 }
 
+void Frame_prof::onChange_notes(wxDataViewEvent &evenement)
+{
+	if(evenement.GetColumn()==-1 || evenement.GetColumn()==0) return ;
+	
+	wxMessageBox("test");
+	
+	
+}
+
+void Frame_prof::onDbclick_notes(wxDataViewEvent &evenement)
+{
+	if(evenement.GetColumn()==-1 || evenement.GetColumn()==0) return ;
+		
+	wxDataViewItem item=liste_notes->GetCurrentItem();
+	wxDataViewColumn *colonne=evenement.GetDataViewColumn();
+	
+	liste_notes->EditItem(item,colonne);
+}
 
 /*
 
