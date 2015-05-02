d0a1a735c78e334b0bacb3ee654263e6adc5b5f9
gusfl@free.fr <gusfl@free.fr>
2015-05-02 14:24:31 +0200

    Added teacher editing backend

--- src/professeur.cpp
+++ src/professeur.cpp
@@ -3,19 +3,18 @@
 Frame_prof::Frame_prof(Frame_login* parent,int& matricule,connexion_bdd*& bdd) : Frame_principale(parent,matricule,bdd)
 {
 	bool premiere_fois=true;
-	int id_matierere_en_cours=-1,id_classe_en_cours=-1;
 	
     wxArrayString texte_choix_matieres;
 	wxArrayString texte_choix_classes;
 	
 	//on parcourt la liste des matières
-	requete_sql *req=bdd->prepare("select matieres.nom, matieres.id_matiere, profs.nom,profs.prenom from profs join matieres on matieres.id_matiere=profs.matiere where profs.id=:matricule");
+	requete_sql *req=bdd->prepare("SELECT matieres.nom, matieres.id_matiere, profs.nom,profs.prenom FROM profs join matieres ON matieres.id_matiere=profs.matiere WHERE profs.id=:matricule");
 	req->bind(":matricule",matricule);
 	while(req->fetch())
 	{
-		if(premiere_fois) //on retient l'id de la première matière car on la séléctionnera
+		if(premiere_fois) //on retient l'id de la première matière car ON la séléctionnera
 		{
-			id_matierere_en_cours=req->getColumn_int(1);
+			id_matiere_en_cours=req->getColumn_int(1);
 			premiere_fois=false;
 		}
 		texte_choix_matieres.Add(wxString(req->getColumn_text(0)));
@@ -24,13 +23,13 @@ Frame_prof::Frame_prof(Frame_login* parent,int& matricule,connexion_bdd*& bdd) :
 	
 	premiere_fois=true;
 	//On séléctionne les classes correspondant à la matière.
-	req=bdd->prepare("select classes.nom, classes.id from profs join classes on classes.id=profs.classe where profs.id=:matricule and profs.matiere=:matiere");
+	req=bdd->prepare("SELECT classes.nom, classes.id FROM profs join classes ON classes.id=profs.classe WHERE profs.id=:matricule AND profs.matiere=:matiere");
 	req->bind(":matricule",matricule);
-	req->bind(":matiere",id_matierere_en_cours);
+	req->bind(":matiere",id_matiere_en_cours);
 	
 	while(req->fetch())
 	{
-		if(premiere_fois) //on retient l'id de la première matière car on la séléctionnera
+		if(premiere_fois) //on retient l'id de la première matière car ON la séléctionnera
 		{
 			id_classe_en_cours=req->getColumn_int(1);
 			premiere_fois=false;
@@ -60,21 +59,21 @@ Frame_prof::Frame_prof(Frame_login* parent,int& matricule,connexion_bdd*& bdd) :
 	liste_notes->AppendTextColumn(_T("Élève"));
 	liste_notes->AppendTextColumn(_T("CE"),wxDATAVIEW_CELL_EDITABLE);
 	liste_notes->AppendTextColumn(_T("TAI"),wxDATAVIEW_CELL_EDITABLE);
+	liste_notes->AppendTextColumn(_T("Projet"),wxDATAVIEW_CELL_EDITABLE);
 	liste_notes->AppendTextColumn(_T("DE"),wxDATAVIEW_CELL_EDITABLE);
 	liste_notes->AppendTextColumn(_T("Commentaires"),wxDATAVIEW_CELL_EDITABLE);
 			
-	req=bdd->prepare("select eleve.id,eleve.nom,eleve.prenom, 0 as pas_de_note, notes.note,notes.type_note from eleve 	\
-															left outer join notes on notes.id_eleve=eleve.id 			\
-							where notes.id_matiere=:matiere																\
+	req=bdd->prepare("SELECT eleve.id,eleve.nom,eleve.prenom, 0 AS pas_de_note, notes.note,notes.type_note FROM eleve 	\
+															LEFT OUTER JOIN notes ON notes.id_eleve=eleve.id 			\
+							WHERE notes.id_matiere=:matiere																\
 					UNION																								\
-					 select eleve.id,eleve.nom,eleve.prenom, 1 as pas_de_note, notes.note,notes.type_note from eleve  	\
-															left outer join notes on notes.id_eleve=eleve.id 			\
-							where eleve.classe=:classe and notes.note is null");
+					 SELECT eleve.id,eleve.nom,eleve.prenom, 1 AS pas_de_note, notes.note,notes.type_note FROM eleve  	\
+															LEFT OUTER JOIN notes ON notes.id_eleve=eleve.id 			\
+							WHERE eleve.classe=:classe AND notes.note is null");
 	
 	req->bind(":classe",id_classe_en_cours);
-	req->bind(":matiere",id_matierere_en_cours);
+	req->bind(":matiere",id_matiere_en_cours);
 	
-	std::map<int,int> liste_eleves;
 	std::map<int,int>::iterator it;
 	wxVector<wxVariant> ligne;//wxWidgets à besoin d'un vecteur pour créer une ligne. Ce vecteurs doit contenir les champs des colonnes de cette ligne
 	
@@ -88,7 +87,7 @@ Frame_prof::Frame_prof(Frame_login* parent,int& matricule,connexion_bdd*& bdd) :
 		texte_note="";
 		
 		it=liste_eleves.find(req->getColumn_int(0));//si la valeur n'est pas trouvée, it sera = a liste_eleves.end()
-		if(it== liste_eleves.end())//if(l'id de la matiere n'est pas dans le vector) vector[id] = la_position_ou_on_ajoute_cette_matiere
+		if(it== liste_eleves.end())//if(l'id de l'éleve n'est pas dans le vector) vector[id] = la_position_ou_on_ajoute_cette_matiere
 		{
 			liste_eleves[req->getColumn_int(0)]=liste_eleves.size()-1;
 			
@@ -102,7 +101,7 @@ Frame_prof::Frame_prof(Frame_login* parent,int& matricule,connexion_bdd*& bdd) :
 			texte_note<<req->getColumn_int(4);
 
 			liste_notes->SetTextValue(texte_note,liste_eleves[req->getColumn_int(0)],position_y);
-			//if(il y a une note a afficher) on affiche la note en (position_x/liste_matiere; position_y/le_type_de_note)
+			//if(il y a une note a afficher) ON affiche la note en (position_x/liste_matiere; position_y/le_type_de_note)
 		}
 	}
 	req->closeCursor();
@@ -136,9 +135,43 @@ void Frame_prof::onChange_notes(wxDataViewEvent &evenement)
 {
 	if(evenement.GetColumn()==-1 || evenement.GetColumn()==0) return ;
 	
-	wxMessageBox("test");
+	int id_eleve,note;
+	bool pas_trouve=true;
+	
+	std::string texte_req;
+	std::map<int,int>::iterator it=liste_eleves.begin();
+	
+	while(pas_trouve && it != liste_eleves.end()) 
+	{
+		if(it->second == liste_notes->GetSelectedRow()) pas_trouve=false;//on récupère l'id de l'élève en cours a partir de la ligne séléctionnée
+		else it++;// (on parcourt la map des élèves pour retrouver l'id)
+	}
+	
+	if(pas_trouve==true) return;//si il y a une erreur interne au programme (c'est pas censé arriver mais bon...)
 	
 	
+	note=wxAtoi(liste_notes->GetTextValue(liste_notes->GetSelectedRow(),evenement.GetColumn()));//et on récupère la note modifiée à partir de la ligne/colonne en cours.
+	id_eleve=it->first;
+	
+	requete_sql *req=bdd->prepare("SELECT count(note) FROM notes WHERE id_eleve=:id_eleve AND id_matiere=:id_matiere AND type_note=:type_note");
+	req->bind(":id_matiere",id_matiere_en_cours);
+	req->bind(":id_eleve",id_eleve);
+	req->bind(":type_note",evenement.GetColumn());//on regarde si cette note existe déja
+	req->fetch();
+	
+	texte_req=(req->getColumn_int(0)==1)?// si c'est pas le cas : on l'ajoute;sinon on la crée.
+		"UPDATE notes set note=:note WHERE id_eleve=:id_eleve AND id_matiere=:id_matiere AND type_note=:type_note":
+		"INSERT INTO notes VALUES (:id_eleve,:id_matiere,:note,:type_note)";
+	req->closeCursor();
+	
+	
+	req=bdd->prepare(texte_req);
+	req->bind(":id_matiere",id_matiere_en_cours);
+	req->bind(":id_eleve",id_eleve);
+	req->bind(":type_note",evenement.GetColumn());
+	req->bind(":note",note);
+	req->fetch();
+	req->closeCursor();
 }
 
 void Frame_prof::onDbclick_notes(wxDataViewEvent &evenement)
